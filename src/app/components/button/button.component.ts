30fcbd711ca74ee6edd80bf8c9894617d5bc5f3a
Vitor Pouza <vhnpouza@gmail.com>
2022-05-05 12:28:09 -0300

    created custom event for button

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'app-button',
@@ -8,9 +9,14 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ButtonComponent implements OnInit {
   @Input() text!: string;
   @Input() color!: string;
+  @Output() btnClick = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onClick() {
+    this.btnClick.emit();
+  }
 }
