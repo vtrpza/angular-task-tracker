968e5cc7eb14680aa882ec4c7c6c4aeac78481cb
Vitor Pouza <vhnpouza@gmail.com>
2022-05-06 18:50:38 -0300

    implemented backend json server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { IconModule } from '@ant-design/icons-angular';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
-import { IconModule } from '@ant-design/icons-angular';
-import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,6 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { TASKS } from '../mock-tasks';
 import { Task } from '../Task';
 
 @Injectable({
@@ -8,10 +8,11 @@ import { Task } from '../Task';
 })
 export class TaskService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  constructor(private http:HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
