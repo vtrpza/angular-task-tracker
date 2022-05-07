5add02f7a47c784235732d22c0bc51e6e2a16fce
Vitor Pouza <vhnpouza@gmail.com>
2022-05-06 22:48:39 -0300

    implemented ui service to add-task component

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/services/ui.service';
 import { Task } from 'src/app/Task';
 
 @Component({
@@ -13,8 +15,12 @@ export class AddTaskComponent implements OnInit {
   text!: string;
   day!: string;
   reminder?: boolean = false;
+  showAddTask?: boolean;
+  subscription!: Subscription
 
-  constructor() { }
+  constructor(private uiService: UiService) {
+    this.subscription =  this.uiService.onToggle().subscribe((value) => (this.showAddTask = value));
+  }
 
   ngOnInit(): void {
   }
